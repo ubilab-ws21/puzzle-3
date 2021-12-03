7c0f08714e5db602cc849f76439d1061637cd8cf
Henrike96 <henrike.koehler@aol.de>
2021-12-03 09:06:17 +0100

    First working version for display: Example code from library. Touched not touched yet^^

---
+++ Code/puzzle_display_control/src/main.cpp
@@ -0,0 +1,78 @@
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_RA8875.h"
+#include "Adafruit_I2CDevice.h"
+
+// LCD
+// Library only supports hardware SPI at this time
+// Connect SCLK to UNO Digital #13 (Hardware SPI clock)
+// Connect MISO to UNO Digital #12 (Hardware SPI MISO)
+// Connect MOSI to UNO Digital #11 (Hardware SPI MOSI)
+#define RA8875_INT     14
+#define RA8875_CS      5
+#define RA8875_RESET   27
+
+Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
+uint16_t tx, ty;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("RA8875 start");
+
+  /* Initialize the display using 'RA8875_480x80', 'RA8875_480x128', 'RA8875_480x272' or 'RA8875_800x480' */
+  if (!tft.begin(RA8875_800x480)) {
+    Serial.println("RA8875 Not Found!");
+    while (1);
+  }
+
+  tft.displayOn(true);
+  tft.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
+  tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
+  tft.PWM1out(255);
+  tft.fillScreen(RA8875_BLACK);
+
+  /* Switch to text mode */
+  tft.textMode();
+  tft.cursorBlink(32);
+
+
+  /* Set a solid for + bg color ... */
+
+  /* ... or a fore color plus a transparent background */
+
+
+  /* Set the cursor location (in pixels) */
+  tft.textSetCursor(10, 10);
+
+  /* Render some text! */
+  char string[15] = "Hello, World! ";
+  tft.textTransparent(RA8875_WHITE);
+  tft.textWrite(string);
+  tft.textColor(RA8875_WHITE, RA8875_RED);
+  tft.textWrite(string);
+  tft.textTransparent(RA8875_CYAN);
+  tft.textWrite(string);
+  tft.textTransparent(RA8875_GREEN);
+  tft.textWrite(string);
+  tft.textColor(RA8875_YELLOW, RA8875_CYAN);
+  tft.textWrite(string);
+  tft.textColor(RA8875_BLACK, RA8875_MAGENTA);
+  tft.textWrite(string);
+
+  /* Change the cursor location and color ... */
+  tft.textSetCursor(100, 100);
+  tft.textTransparent(RA8875_RED);
+  /* If necessary, enlarge the font */
+  tft.textEnlarge(1);
+  /* ... and render some more text! */
+  tft.textWrite(string);
+  tft.textSetCursor(100, 150);
+  tft.textEnlarge(2);
+  tft.textWrite(string);
+}
+
+void loop()
+{
+}
