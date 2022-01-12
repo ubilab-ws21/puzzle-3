c4fa8be887d8cc781b017ba4021eab98e5693fef
Henrike96 <henrike.koehler@aol.de>
2022-01-12 18:04:15 +0100

    corrected term mistake

--- Code/puzzle_display_control/include/encoder_handle.h
+++ Code/puzzle_display_control/include/encoder_handle.h
@@ -6,7 +6,7 @@
 void init_encoder(void);
 int check_encoder(void);
 
-int encoder_get_value(int encoder_value);
+int encoder_get_value(int encoder_num);
 void encoder_set_value(int encoder_num, int value);
 
 #endif
